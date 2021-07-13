44f7706cf0b2202613ca3d5422a916b4160670b1
Albina Mashlyakevich <AlbinaMashlyakevich@sebrands.com>
2021-07-13 08:10:21 +0300

    Task 120 Testing via SauceLab

--- Task70/YandexTests.cs
+++ Task70/YandexTests.cs
@@ -1,40 +1,19 @@
-using Allure.Commons;
 using Allure.Commons.Model;
 using Allure.NUnit.Attributes;
 using NUnit.Framework;
-using OpenQA.Selenium.Chrome;
-using System;
-using System.IO;
 using Task70.YandexPages;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Remote;
 
 namespace Task70
 {
     [TestFixture]
-    public class Tests: AllureReport
+    [Parallelizable]
+    [TestFixture("MicrosoftEdge", "latest", "Windows 10")]
+    [TestFixture("firefox", "55.0", "Windows 8.1")]
+    [TestFixture("chrome", "latest", "macOS 10.13")]
+    public class Tests: Base
     {
-        private IWebDriver driver { get; set; }
-        private string nodeURL;
-
-        [OneTimeSetUp]
-        public void SetupForAllure()
+        public Tests(string browser, string version, string os) : base(browser, version, os)
         {
-            Environment.CurrentDirectory = Path.GetDirectoryName(GetType().Assembly.Location);
-        }
-
-        [SetUp]
-        public void Setup()
-        {
-            nodeURL = "http://localhost:4444/wd/hub";
-            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-            desiredCapabilities.SetCapability(CapabilityType.BrowserName, "chrome");
-            desiredCapabilities.SetCapability(CapabilityType.Platform, new Platform(PlatformType.WinNT));
-            desiredCapabilities.SetCapability(CapabilityType.BrowserName, "firefox");
-            desiredCapabilities.SetCapability(CapabilityType.Platform, new Platform(PlatformType.WinNT));
-            driver = new RemoteWebDriver(new Uri(nodeURL), desiredCapabilities);
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
         }
 
         [Test]
@@ -74,42 +53,6 @@ namespace Task70
             homePage.ClickUsernameLink();
             homePage.ClickSignoutButton();
             homePage.TakeScreenshot("Logout Test");
-        }
-
-        [TearDown]
-        public void CleanUp()
-        {
-            if (TestContext.CurrentContext.Result.FailCount > 0)
-            {
-                TakeScreenshot("Test is failed");
-            }
-            driver.Close();
-        }
-
-        [OneTimeTearDown]
-        public void TearDown()
-        {
-            if (driver != null)
-            {
-                driver.Quit();
-            }
-        }
-
-        public void TakeScreenshot(string testCase)
-        {
-            try
-            {
-                var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
-
-                var fileName = @"Screenshot " + testCase + " " + DateTime.Now.ToString("HH_mm_ss") + ".png";
-                var fileLocation = Path.Combine(@"D:\Screenshots\", fileName);
-
-                screenshot.SaveAsFile(fileLocation, ScreenshotImageFormat.Png);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
         }
     }
 }
